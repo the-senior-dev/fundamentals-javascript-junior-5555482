bdf80dfce3640f09182f802e185b5ef7e7b7c5ff
Nataliia Antonova <5555482@gmail.com>
2026-02-27 18:26:12 +0000

    js-junior

--- src/05_logic_control_flow/findFactorial.js
+++ src/05_logic_control_flow/findFactorial.js
@@ -7,7 +7,10 @@
 // The factorial of 3 is 3 * 2 * 1 = 6.
 // Recommended: use a while of for loop to solve the problem
 function findFactorial(n) {
-    // Your code here
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * findFactorial(n - 1);
 }
 
 module.exports = findFactorial;
